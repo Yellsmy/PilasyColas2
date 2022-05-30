8ae9a616c0aa3f173aace3d680741391c4f925ca
Yellsmy <yltojg@alumno.uspg.edu.gt>
2022-05-29 18:57:41 -0600

    Nodo para ejercicio 3 y 4

---
+++ src/pilasycolas/NodoStringA.java
@@ -0,0 +1,13 @@
+
+package pilasycolas;
+
+//Nodo para ejercicio 1
+public class NodoStringA {
+    NodoStringA siguienteA;
+    char datoS;
+    
+    public NodoStringA(char datoS){
+        this.datoS = datoS;
+        siguienteA = null; 
+    }   
+}
